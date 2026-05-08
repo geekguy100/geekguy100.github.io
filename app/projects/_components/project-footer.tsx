b6e7393171133e935e5b861f4786ce4e8713c8bc
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-08 00:13:06 -0700

    Added defaults for project footer

--- app/projects/_components/project-footer.tsx
+++ app/projects/_components/project-footer.tsx
@@ -1,14 +1,26 @@
 import { cn } from "@/lib/utils"
+import Link from "next/link"
 import type { ComponentPropsWithoutRef, ReactNode } from "react"
 
+type DefaultProjectFooterProps = { children?: never; href: string }
+type CustomProjectFooterProps = { children: ReactNode; href?: never }
+
 export function ProjectFooter({
-  children,
   className,
   ...props
-}: ComponentPropsWithoutRef<"div"> & { children: ReactNode }) {
+}: Omit<ComponentPropsWithoutRef<"div">, "children"> & (DefaultProjectFooterProps | CustomProjectFooterProps)) {
   return (
     <div {...props} className={cn("flex flex-col items-center italic", className)}>
-      {children}
+      {props.href && (
+        <p>
+          For a full list of documents, forms, assets, etc., please click{" "}
+          <Link href={props.href} target="_blank" className="underline">
+            here
+          </Link>
+          .
+        </p>
+      )}
+      {props.children && props.children}
     </div>
   )
 }
