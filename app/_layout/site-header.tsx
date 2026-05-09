a989584d4d09df0ea0464046a005eeb47e1d0641
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-09 16:59:12 -0700

    Re-added the SiteLogo component and added state to it to make sure the intro animation only plays once.

--- app/_layout/site-header.tsx
+++ app/_layout/site-header.tsx
@@ -1,7 +1,6 @@
 import { SocialGrid } from "./social-grid"
 import { NavigationBar } from "./nav-bar"
-import Image from "next/image"
-import HatLogo from "@/public/hat.png"
+import { SiteLogo } from "./site-logo"
 
 export function SiteHeader() {
   // Defaulting header height to what it is when the Image is fully loaded.
@@ -9,15 +8,7 @@ export function SiteHeader() {
     <header className="site-section min-h-43.75 text-center sm:grid sm:grid-cols-3">
       {/* Logo */}
       <div className="flex flex-col items-center justify-center sm:items-end">
-        <Image
-          unoptimized
-          width={175}
-          src={HatLogo}
-          style={{ animationComposition: "add" }}
-          alt="A black hat with wings"
-          className="-my-6 sm:my-0 sm:animate-header-enter"
-        />
-        <ConstructionBanner />
+        <SiteLogo />
       </div>
       {/* Name */}
       <div className="flex flex-col items-center justify-center gap-2">
@@ -30,30 +21,5 @@ export function SiteHeader() {
       {/* Socials */}
       <SocialGrid />
     </header>
-  )
-}
-
-function ConstructionBanner() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 30 24"
-      stroke="black"
-      strokeWidth="1"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="absolute left-2 size-60 -rotate-45 max-sm:-top-2"
-    >
-      <defs>
-        <pattern id="star" viewBox="0,0,5,5" width="20%" height="100%">
-          <line x1={0} y1={-3} x2={10} y2={20} />
-        </pattern>
-      </defs>
-      <rect x="2" y="6" width="24" height="8" rx="1" fill="yellow" />
-      <rect x="2" y="6" width="24" height="8" rx="1" stroke="none" fill="url(#star)" />
-      <text x={5} y={13} strokeWidth="0.25" stroke="black" fill="white" style={{ font: "8px sans-serif" }}>
-        W.I.P
-      </text>
-    </svg>
   )
 }
---
+++ app/_layout/site-logo.tsx
@@ -0,0 +1,50 @@
+"use client"
+import HatLogo from "@/public/hat.png"
+import Image from "next/image"
+import { useState } from "react"
+
+export function SiteLogo() {
+  const [animPlayed, setAnimPlayed] = useState(false)
+  return (
+    <div className="flex flex-col items-center justify-center sm:items-end">
+      <Image
+        unoptimized
+        width={175}
+        src={HatLogo}
+        style={{ animationComposition: "add" }}
+        alt="A black hat with wings"
+        className={`-my-6 sm:my-0 ${!animPlayed && "sm:animate-header-enter"}`}
+        onAnimationEnd={(ev) => {
+          if (ev.animationName !== "header-enter") return
+          return setAnimPlayed(true)
+        }}
+      />
+      <ConstructionBanner />
+    </div>
+  )
+}
+
+function ConstructionBanner() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 30 24"
+      stroke="black"
+      strokeWidth="1"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="absolute left-2 size-60 -rotate-45 max-sm:-top-2"
+    >
+      <defs>
+        <pattern id="star" viewBox="0,0,5,5" width="20%" height="100%">
+          <line x1={0} y1={-3} x2={10} y2={20} />
+        </pattern>
+      </defs>
+      <rect x="2" y="6" width="24" height="8" rx="1" fill="yellow" />
+      <rect x="2" y="6" width="24" height="8" rx="1" stroke="none" fill="url(#star)" />
+      <text x={5} y={13} strokeWidth="0.25" stroke="black" fill="white" style={{ font: "8px sans-serif" }}>
+        W.I.P
+      </text>
+    </svg>
+  )
+}
