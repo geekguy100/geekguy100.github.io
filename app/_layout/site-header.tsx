3197416b77a75cfa9a325b7799d738126b6f7e6f
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-09 02:42:11 -0700

    Added WIP banner to site header

--- app/_layout/site-header.tsx
+++ app/_layout/site-header.tsx
@@ -18,6 +18,7 @@ export function SiteHeader() {
           alt="A black hat with wings"
           className="-my-6 sm:my-0 sm:animate-header-enter"
         />
+        <ConstructionBanner />
       </div>
       {/* Name */}
       <div className="flex flex-col items-center justify-center gap-2">
@@ -30,5 +31,30 @@ export function SiteHeader() {
       {/* Socials */}
       <SocialGrid />
     </header>
+  )
+}
+
+function ConstructionBanner() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 30 24"
+      stroke="black"
+      strokeWidth="1"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="absolute left-2 size-60 -rotate-45 max-sm:-top-2"
+    >
+      <defs>
+        <pattern id="star" viewBox="0,0,5,5" width="20%" height="100%">
+          <line x1={0} y1={-3} x2={10} y2={20} />
+        </pattern>
+      </defs>
+      <rect x="2" y="6" width="24" height="8" rx="1" fill="yellow" />
+      <rect x="2" y="6" width="24" height="8" rx="1" stroke="none" fill="url(#star)" />
+      <text x={5} y={13} strokeWidth="0.25" stroke="black" fill="white" style={{ font: "8px sans-serif" }}>
+        W.I.P
+      </text>
+    </svg>
   )
 }
