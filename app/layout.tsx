a28877573a35b57d1b1724f54c49f19ed648ced7
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-09 14:10:31 -0700

    Turning preload off for fonts since Next.js says they are not being used within a few seconds.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,12 @@ import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils"
 import { SiteHeader } from "./_layout/site-header"
 
-const roboto = Roboto({ subsets: ["latin"], variable: "--font-sans" })
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-sans", preload: false })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
+  preload: false,
 })
 
 export default function RootLayout({
