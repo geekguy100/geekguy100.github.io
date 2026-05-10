58004c33642f36aa3a568411e212b763cdf74603
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-09 18:02:02 -0700

    Removed unused layout.tsx from projects folder

--- app/projects/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { Container } from "@/components/containers"
-import type { PropsWithChildren } from "react"
-
-export default async function ProjectsLayout({ children }: PropsWithChildren) {
-  return children
-}
