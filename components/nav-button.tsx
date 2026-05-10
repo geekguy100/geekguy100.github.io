9a23b68c2b9fdc4113dfd22ac0f470afdd516e5e
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-09 18:35:05 -0700

    Support for smooth scrolling to anchors on a page

--- components/nav-button.tsx
+++ components/nav-button.tsx
@@ -1,18 +1,50 @@
+"use client"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-import type { ComponentProps, ComponentPropsWithoutRef, CustomComponentPropsWithRef, ReactNode } from "react"
+import { useCallback, type ComponentProps, type MouseEvent, type ReactNode } from "react"
 
 export type NavButtonProps = {
   href: string
   children: ReactNode
 } & Omit<ComponentProps<typeof Button>, "asChild" | "children"> & { newTab?: boolean }
 
-export function NavigationButton({ href, children, newTab = false, ...props }: NavButtonProps) {
+export function NavigationButton({ href, children, onClick, newTab = false, ...props }: NavButtonProps) {
+  const clickCallback = useCallback(
+    (ev: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
+      onClick?.(ev)
+      if (newTab) return
+
+      // If we were able to smooth scroll, prevent default behavior
+      // or else the scroll will be instant.
+      if (trySmoothScroll(href)) {
+        ev.preventDefault()
+      }
+    },
+    [href, onClick, newTab]
+  )
+
   return (
-    <Button {...props} asChild>
+    <Button {...props} onClick={clickCallback} asChild>
       <Link target={newTab ? "_blank" : undefined} href={href}>
         {children}
       </Link>
     </Button>
   )
+}
+
+function trySmoothScroll(href: string): boolean {
+  const id = getAnchorId(href)
+  if (id === null) return false
+
+  const target = document.getElementById(id)
+  if (!target) return false
+  target.scrollIntoView({ behavior: "smooth" })
+  return true
+}
+
+function getAnchorId(href: string) {
+  const index = href.indexOf("#")
+  if (index < 0) return null
+
+  return href.slice(index + 1)
 }
