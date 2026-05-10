bfe4ec292f4d850b6fb5183192d4c653d491d89a
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-10 01:54:19 -0700

    Added support for overriding the className of the ProjectDescription component

--- app/projects/_components/project-intro.tsx
+++ app/projects/_components/project-intro.tsx
@@ -1,4 +1,5 @@
 import { SectionTitle } from "@/components/section-title"
+import { cn } from "@/lib/utils"
 import type { PropsWithChildren } from "react"
 
 export function ProjectIntro({ children }: PropsWithChildren) {
@@ -9,6 +10,6 @@ export function ProjectTitle({ children }: PropsWithChildren) {
   return <SectionTitle className="capitalize">{children}</SectionTitle>
 }
 
-export function ProjectDescription({ children }: PropsWithChildren) {
-  return <p className="text-left">{children}</p>
+export function ProjectDescription({ className, children }: PropsWithChildren & { className?: string }) {
+  return <p className={cn("text-left", className)}>{children}</p>
 }
