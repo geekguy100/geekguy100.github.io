741499598acb935c37aadad8164c8901f6dfba35
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-13 07:39:29 -0700

    chore: removed Container component since it was not used

--- components/containers.tsx
+++ components/containers.tsx
@@ -1,18 +1,8 @@
 import { cn } from "@/lib/utils"
 import type { ComponentProps, HTMLAttributes } from "react"
 
-const COMMON_CLASSNAME: HTMLAttributes<"div">["className"] = "site-section flex flex-col p-6 *:lg:mx-48"
+const COMMON_CLASSNAME: HTMLAttributes<"article">["className"] = "site-section flex flex-col p-6 *:lg:mx-48"
 
-export function Article(props: ComponentProps<"article">) {
-  return <Container type="article" {...props} />
-}
-
-export function Container<T extends keyof React.JSX.IntrinsicElements>({
-  type,
-  className,
-  ...props
-}: ComponentProps<T> & { type: T }) {
-  // Have to bypass some TypeScript here to prevent errors, but React allows any intrinsic element provided as a string to be used as a component.
-  const Container = type as unknown as "div"
-  return <Container className={cn(COMMON_CLASSNAME, className)} {...(props as unknown as ComponentProps<"div">)} />
+export function Article({ className, ...props }: ComponentProps<"article">) {
+  return <article className={cn(COMMON_CLASSNAME, className)} {...props} />
 }
