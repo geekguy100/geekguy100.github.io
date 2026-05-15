5bbbd55f82ebd53a0b711e8064e6bacc9736a093
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-14 20:40:15 -0700

    chore: replaced older custom slide in animation for About Me desc. with motion animation

--- app/(home)/_sections/aboutme.tsx
+++ app/(home)/_sections/aboutme.tsx
@@ -1,31 +1,32 @@
 "use client"
 import { ArticleWithIntro } from "@/components/section-with-intro"
+import { slideInProps } from "@/lib/animation"
+import { motion } from "motion/react"
 
 // Setting this outside of the component will make sure the animation only plays once during the session.
-let animPlayed = false
 export default function AboutMe() {
   return (
     <ArticleWithIntro title="About Me" id="aboutme">
-      <div onAnimationEnd={() => (animPlayed = true)} className={animPlayed ? "" : "group-slide"}>
-        <p className="[--delay-index:0]">
+      <motion.div {...slideInProps(0)}>
+        <motion.p {...slideInProps(1)}>
           Hey there! I'm Kyle Grenier, a graduate of Bradley University and an immersive technology developer for The
           Aerospace Corporation based in South California.
-        </p>
-        <p className="[--delay-index:1]">
+        </motion.p>
+        <motion.p {...slideInProps(2)}>
           Ever since I was young, I have loved technology in all aspects, from robotics and science fiction to computers
           and programming. I'm the go-to IT person in my house, and as a triplet, I am definitely the most curious of
           the three.
-        </p>
-        <p className="[--delay-index:2]">
+        </motion.p>
+        <motion.p {...slideInProps(3)}>
           It was in elementary school that I started tinkering around with game development in GameMaker, and in middle
           school more advanced programming and the Unity game engine. For the longest time, I've known that I wanted to
           go into the video game development industry. I love seeing my work evolve over time and the reactions people
           have when playing my games.
-        </p>
-        <p className="[--delay-index:3]">
+        </motion.p>
+        <motion.p {...slideInProps(4)}>
           Although programming is my specialty, I also enjoy creating electronic music in my free time.
-        </p>
-      </div>
+        </motion.p>
+      </motion.div>
     </ArticleWithIntro>
   )
 }
