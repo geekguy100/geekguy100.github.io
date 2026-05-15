38dd909dfda3e00093e29865ec7d9e6939dc640e
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-14 22:52:21 -0700

    chore: deleted obsolete animation code

--- lib/animation.ts
+++ lib/animation.ts
@@ -37,24 +37,3 @@ export const childSlideInProps = {
   variants: childSlideInVariants,
 }
 //#endregion
-
-// Legacy
-export function slideInProps(index: number = 0, once: boolean = true) {
-  return {
-    initial: "hidden",
-    whileInView: "visible",
-    variants: slideInVariants,
-    custom: index,
-    viewport: { once },
-    inherit: true,
-  } satisfies CustomComponentPropsWithRef<typeof motion.div>
-}
-
-export const slideInVariants = {
-  hidden: { opacity: 0, translateX: -50 },
-  visible: (index: number = 0) => ({
-    opacity: 1,
-    translateX: 0,
-    transition: { delay: 0.05 * index },
-  }),
-} satisfies Variants
