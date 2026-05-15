e5bc84036aa5cbd8c8ec6435185b45080586dfb0
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-14 23:06:47 -0700

    fix: fixed styling issue in custom Article component

--- components/containers.tsx
+++ components/containers.tsx
@@ -1,14 +1,15 @@
 "use client"
 import { containerSlideInProps } from "@/lib/animation"
+import { cn } from "@/lib/utils"
 import { motion } from "motion/react"
 import type { CustomComponentPropsWithRef, HTMLAttributes } from "react"
 
-const COMMON_CLASSNAME: HTMLAttributes<"article">["className"] = "site-section flex flex-col p-6 *:lg:mx-48"
+const COMMON_CLASSNAME: HTMLAttributes<"article">["className"] = "flex flex-col p-6 *:lg:mx-48"
 
-export function Article(props: CustomComponentPropsWithRef<typeof motion.article>) {
+export function Article({ className, ...props }: CustomComponentPropsWithRef<typeof motion.article>) {
   return (
-    <div className={COMMON_CLASSNAME}>
-      <motion.article {...containerSlideInProps} {...props} />
+    <div className="site-section">
+      <motion.article {...containerSlideInProps} {...props} className={cn(COMMON_CLASSNAME, className)} />
     </div>
   )
 }
