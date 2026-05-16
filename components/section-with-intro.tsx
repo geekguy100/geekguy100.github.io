b050a3ac48bbd1a23a4c41195de1c3643c51a12c
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-15 18:06:34 -0700

    chore: added support for passing additional props into the ArticleWithIntro component

--- components/section-with-intro.tsx
+++ components/section-with-intro.tsx
@@ -1,28 +1,28 @@
 "use client"
-import type { PropsWithChildren, ReactNode } from "react"
+import type { CustomComponentPropsWithRef, PropsWithChildren, ReactNode } from "react"
 import { Article } from "./containers"
 import { SECTION_MARGIN_BOTTOM, SectionTitle } from "./section-title"
 import { childFadeInVariants, containerSlideInProps } from "@/lib/animation"
-import { motion } from "motion/react"
 
-export interface ShowcaseSectionProps extends PropsWithChildren {
+export type ShowcaseSectionProps = Omit<
+  CustomComponentPropsWithRef<typeof Article>,
+  "children" | "id" | "className"
+> & {
+  children: ReactNode
+} & {
   title: string
   description?: ReactNode
   id: string
 }
 
-export function ArticleWithIntro({ title, description, id, children }: ShowcaseSectionProps) {
+export function ArticleWithIntro({ title, description, id, children, ...props }: ShowcaseSectionProps) {
   return (
-    <Article {...containerSlideInProps} className="relative" id={id}>
+    <Article {...containerSlideInProps} className="relative" id={id} {...props}>
       <div className={`text-center ${description && SECTION_MARGIN_BOTTOM}`}>
         <SectionTitle variants={childFadeInVariants} className={description !== undefined ? "mb-0" : ""}>
           {title}
         </SectionTitle>
-        {typeof description === "string" ? (
-          <motion.p variants={childFadeInVariants}>{description}</motion.p>
-        ) : (
-          description
-        )}
+        {typeof description === "string" ? <p>{description}</p> : description}
       </div>
       {children}
     </Article>
