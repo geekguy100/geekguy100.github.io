770122f32cc88722e86fe2bd2f184b97f47b2332
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-17 21:59:56 -0700

    chore: additional content on unreal-cesium page

--- app/projects/unreal-cesium/page.tsx
+++ app/projects/unreal-cesium/page.tsx
@@ -17,40 +17,42 @@ export default function UnrealCesiumPage() {
     <Project>
       <ProjectIntro>
         <ProjectTitle>Cislunar Space Visualization Tool</ProjectTitle>
-        <ProjectDescription>
-          This project was my first dive into Unreal Engine. It involved using NASA's SPICE toolkit (via the
-          NASA-acknowledged{" "}
-          <Link
-            className="underline"
-            href="https://www.fab.com/listings/8b599b16-39bf-41bd-9ea5-3d1f70d45d06"
-            target="_blank"
-          >
-            MaxQ Spaceflight Toolkit
-          </Link>{" "}
-          plugin) to propagate satellites and the{" "}
-          <Link className="underline" href="https://cesium.com/platform/cesium-for-unreal/" target="_blank">
-            Cesium for Unreal Engine
-          </Link>{" "}
-          plugin to present realistic Earth and Moon Actors.
-        </ProjectDescription>
-        <ProjectDescription>
-          Additionally, data from other internal tools were fed into the application to add ground stations and landers
-          onto the surfaces of the Earth and Moon.
-        </ProjectDescription>
-        <ProjectDescription>
-          This application was a contribution to the larger{" "}
-          <Link
-            className="underline"
-            target="_blank"
-            href="https://aerospace.org/article/simulating-cislunar-space-why-experts-want-construct-digital-moon"
-          >
-            Aerospace Cislunar Modeling Environment (ACME)
-          </Link>{" "}
-          project.
-        </ProjectDescription>
-        <ProjectDescription className="ml-4 italic">
-          Please note that, unfortunately, no photos or videos are available publicly
-        </ProjectDescription>
+        <div className="space-y-2">
+          <ProjectDescription>
+            This project was my first dive into Unreal Engine. It involved using NASA's SPICE toolkit (via the
+            NASA-acknowledged{" "}
+            <Link
+              className="underline"
+              href="https://www.fab.com/listings/8b599b16-39bf-41bd-9ea5-3d1f70d45d06"
+              target="_blank"
+            >
+              MaxQ Spaceflight Toolkit
+            </Link>{" "}
+            plugin) to propagate satellites and the{" "}
+            <Link className="underline" href="https://cesium.com/platform/cesium-for-unreal/" target="_blank">
+              Cesium for Unreal Engine
+            </Link>{" "}
+            plugin to present realistic Earth and Moon Actors.
+          </ProjectDescription>
+          <ProjectDescription>
+            Additionally, data from other internal tools were fed into the application to add ground stations and
+            landers onto the surfaces of the Earth and Moon.
+          </ProjectDescription>
+          <ProjectDescription>
+            This application was a contribution to the larger{" "}
+            <Link
+              className="underline"
+              target="_blank"
+              href="https://aerospace.org/article/simulating-cislunar-space-why-experts-want-construct-digital-moon"
+            >
+              Aerospace Cislunar Modeling Environment (ACME)
+            </Link>{" "}
+            project.
+          </ProjectDescription>
+          <ProjectDescription className="italic">
+            Please note that, unfortunately, no photos or videos of my work are available publicly.
+          </ProjectDescription>
+        </div>
       </ProjectIntro>
 
       {/* TODO: Add in a "what i'd do differently" */}
@@ -65,29 +67,40 @@ export default function UnrealCesiumPage() {
             <Responsibility>
               Created application lifecycle
               <Responsibilities>
-                <Responsibility>Loaders to ingest data from different sources</Responsibility>
                 <Responsibility>
-                  Factories to create commands that spawned and initialized Actors of different types, depending on the
-                  ingested data
+                  Design patterns used include abstract factory pattern, strategy pattern, command pattern, and template
+                  method pattern, among others.
+                  <Responsibilities>
+                    <Responsibility>Loaders to ingest data from different sources.</Responsibility>
+                    <Responsibility>
+                      Factories to create commands that spawned and initialized Actors of different types, depending on
+                      the ingested data.
+                    </Responsibility>
+                  </Responsibilities>
                 </Responsibility>
               </Responsibilities>
             </Responsibility>
 
             <Responsibility>
-              UI to display date and time of the simulation
+              UI to display date and time of the simulation.
               <Responsibilities>
-                <Responsibility>Support for speeding up, slowing down, and rewinding the simulation</Responsibility>
+                <Responsibility>Support for speeding up, slowing down, and rewinding the simulation.</Responsibility>
               </Responsibilities>
             </Responsibility>
 
-            <Responsibility>Support for displaying orbit trails of satellites</Responsibility>
+            <Responsibility>Support for displaying orbit trails of satellites.</Responsibility>
 
             <Responsibility>
               Utilized the MaxQ Spaceflight Toolkit plugin to accurately position and orient the Earth and Moon
-              according to the date and time of the simulation
+              according to the date and time of the simulation.
             </Responsibility>
 
-            <Responsibility>Participated in final presentation to over 30 people</Responsibility>
+            <Responsibility>
+              Collaborated with orbital mechanics subject matter experts when I required assistance confirming orbits,
+              Actor positions, etc.
+            </Responsibility>
+
+            <Responsibility>Participated in final presentation to over 30 people.</Responsibility>
           </Responsibilities>
         </ProjectRole>
       </ProjectRoles>
