1313d6b46cdac11928b1577cfd1802fff0a6074f
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-17 20:21:02 -0700

    feat: embedded itch.io games automatically disappear if they will overflow along the x-axis

--- app/projects/_components/embedded-project.tsx
+++ app/projects/_components/embedded-project.tsx
@@ -4,12 +4,26 @@ import { NavigationButton } from "@/components/nav-button"
 import { childFadeInVariants } from "@/lib/animation"
 import { LocalDownload, type EmbeddedDemoProps } from "./play-project"
 import { useScreenBreakpoint } from "@/hooks/use-mobile"
+import { useCallback, useState } from "react"
 
 export function EmbeddedProject({ children, itchLink, ...props }: EmbeddedDemoProps) {
-  const screenTooSmall = useScreenBreakpoint(1024)
+  const [bp, setBp] = useState<number>(1024)
+  const screenTooSmall = useScreenBreakpoint(bp)
+
+  const setBreakpoint = useCallback((wrapper: HTMLDivElement | null) => {
+    if (wrapper === null) return
+    const iframe = wrapper.querySelector("iframe")
+    if (iframe === null) return
+    setBp(iframe.clientWidth + 100)
+  }, [])
+
   return (
     <MotionDiv variants={childFadeInVariants} className="flex flex-col items-center gap-2">
-      {!screenTooSmall && <div className="overflow-scroll max-lg:hidden">{children}</div>}
+      {!screenTooSmall && (
+        <div ref={setBreakpoint} className="max-lg:hidden">
+          {children}
+        </div>
+      )}
       <LocalDownload {...props}>
         {itchLink && (
           <NavigationButton newTab href={itchLink}>
