6b25324db161c415a9a375cc72a3759258c36e53
Kyle Grenier <kyle.grenier11@gmail.com>
2026-09-10 21:55:52 -0700

    feat: TSCodeBlock component now supports motion animation
    Now uses the <motion.pre/> component instead of <pre/> and uses the childSlideInVariants by default.

--- components/ts-code-block.tsx
+++ components/ts-code-block.tsx
@@ -4,15 +4,18 @@ import "highlight.js/styles/atom-one-dark-reasonable.min.css"
 import hljs from "highlight.js/lib/core"
 import typescript from "highlight.js/lib/languages/typescript"
 import { useEffect, useRef } from "react"
+import { motion, type Variants } from "motion/react"
+import { childSlideInVariants } from "@/lib/animation"
 
 export interface CodeBlockProps {
   children: string
   className?: string
+  variants?: Variants
 }
 
 hljs.registerLanguage("typescript", typescript)
 
-export function TSCodeBlock({ className, children }: CodeBlockProps) {
+export function TSCodeBlock({ className, children, variants }: CodeBlockProps) {
   const codeRef = useRef<HTMLElement | null>(null)
 
   useEffect(() => {
@@ -21,10 +24,10 @@ export function TSCodeBlock({ className, children }: CodeBlockProps) {
   }, [])
 
   return (
-    <pre className={className}>
+    <motion.pre variants={variants ?? childSlideInVariants} className={className}>
       <code ref={codeRef} className="language-typescript">
         {children}
       </code>
-    </pre>
+    </motion.pre>
   )
 }
